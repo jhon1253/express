b66ef6a1802117b5e8eecd722d8db0f670e103cd
jhon1253 <sebastianquinayas@gmail.com>
2024-08-22 11:50:17 -0500

    exprress

--- pro_express/index.js
+++ pro_express/index.js
@@ -6,15 +6,28 @@ const routerUsuarios = require("./src/routes/usuariosRoute");
 const routerVentas = require("./src/routes/ventasRoute");
 const routerFacturas = require("./src/routes/facturaVentasRoute");
 const routerCarrito = require("./src/routes/carritoComprasRoute");
+const loggerMiddelware = require("./src/middelwares/loggerMiddelware")
 
 const { ConexionDB } = require("./src/config/database");
 
 ConexionDB();
 
 const app = express();
+
+
+app.use(cors())
+// app.use(cors({
+//   origin: ['https://example.com','https://example2.com' ]
+// }))
+
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(loggerMiddelware);
+
 
 app.use("/usuarios", routerUsuarios);
 app.use("/ventas", routerVentas);
